b70ef338d531e22e6ff5d9564765c5b98efd3b7f
malavikaac <acmalavika@gmail.com>
2023-04-20 15:00:18 +0530

    first commit

---
+++ lib/basics.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class MyVideoPlayer extends StatefulWidget {
+  const MyVideoPlayer({ Key? key }) : super(key: key);
+
+  @override
+  _MyVideoPlayerState createState() => _MyVideoPlayerState();
+}
+
+class _MyVideoPlayerState extends State<MyVideoPlayer> {
+
+
+  VideoPlayerController? controller;
+
+  String dataSource = "https://shivanesboutique.com/pub/media/S_MAM_VIDEO_10042021.mp4";
+   String asset = "assets/video.mp4";
+
+  @override
+  void initState() {
+    super.initState();
+    controller = VideoPlayerController.asset(asset)..initialize().then((_) {
+      setState(() {
+      });
+    });
+
+
+   controller!.play();
+  }
+
+
+  @override
+  void dispose() {
+    controller!.dispose();
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Video Player ",style: TextStyle(color: Colors.black),),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 200, 252, 252),
+      ),
+      body: ListView(
+        children: [
+          controller!.value.isInitialized ?
+          AspectRatio(aspectRatio: controller!.value.aspectRatio , 
+          child: VideoPlayer(controller!),
+          )
+          :
+          Container(height: 200,  width: double.infinity, child: Center(child: CircularProgressIndicator(),),)
+        ],
+      ),
+    
+    floatingActionButton: FloatingActionButton(
+      backgroundColor:  Color.fromARGB(255, 200, 252, 252),
+      onPressed: () {
+        setState(() {
+          if(controller!.value.isPlaying){
+            controller!.pause();
+          }else{
+            controller!.play();
+          }
+        });
+      },
+      child: Icon( 
+        controller!.value.isPlaying ?
+        Icons.pause:
+        Icons.play_arrow,
+        color:Colors.black ,
+       ),
+    ),
+    
+    )
+    ;
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:player/basics.dart';
+
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home:MyVideoPlayer(),
+      debugShowCheckedModeBanner: false
+    );
+  }
+}
+
+
